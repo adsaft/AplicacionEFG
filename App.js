58ec0dc775a56cdc783602cf71054a5aadb832ff
Miguel Angel Yuen Alvarado <miguel.yuen.04@hotmail.com>
2022-10-31 13:07:35 -0600

    pruebas de poner imange

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image} from 'react-native';
 
-export default class HomeActivity extends Component {
+export default class Menu extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -13,9 +13,18 @@ export default class HomeActivity extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 50,
     flex: 1,
     justifyContent: 'center',
     backgroundColor: '#E1BEE7',
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  logo: {
+    width: 66,
+    height: 58,
   },
   headerText: {
     fontSize: 20,
@@ -24,3 +33,19 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
 });
+/*<Image
+style={styles.tinyLogo}
+source={require('@expo/snack-static/react-native-logo.png')}
+/>
+<Image
+style={styles.tinyLogo}
+source={{
+  uri: 'https://reactnative.dev/img/tiny_logo.png',
+}}
+/>
+<Image
+style={styles.logo}
+source={{
+  uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
+}}
+/>*/
